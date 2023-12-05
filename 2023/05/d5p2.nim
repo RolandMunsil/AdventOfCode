7e9e0fbc0ca2e8adfbcdc6c592600ca2e3d83e80
Roland Munsil <roland@munsil.com>
2023-12-05 00:05:48 -0800

    d5p2: move duplicated logic into proc

--- 2023/05/d5p2.nim
+++ 2023/05/d5p2.nim
@@ -7,11 +7,54 @@ type Range = tuple[start, len: int]
 type RangeMap = tuple[destStart, srcStart, len: int]
 
 let lines = lines("input.txt").toSeq
-
 var seedNums = lines[0].split(':')[1].splitWhitespace.mapIt(parseInt(it))
 
 var sourceRanges: seq[Range] = (0..<(seedNums.len div 2)).mapIt((seedNums[it * 2], seedNums[it * 2 + 1]))
 var rangeMaps: seq[RangeMap]
+
+proc mapSourceRanges() =
+    # split the ranges so that every range is fully within a range map
+
+    var unsplitRanges = sourceRanges
+    var splitRanges: seq[Range]
+
+    while unsplitRanges.len > 0:
+        let srcRange = unsplitRanges.pop
+
+        var split = false
+
+        for rm in rangeMaps:
+            let srcRangeEnd = srcRange.start + srcRange.len
+            let rmSrcEnd = rm.srcStart + rm.len
+
+            # range crosses low end of range map
+
+            if srcRange.start < rm.srcStart and srcRangeEnd > rm.srcStart:
+                unsplitRanges.add (srcRange.start, rm.srcStart - srcRange.start)
+                unsplitRanges.add (rm.srcStart, srcRangeEnd - rm.srcStart)
+                split = true
+                break
+
+            # range crosses high end of range map
+        
+            if srcRange.start < rmSrcEnd and srcRangeEnd > rmSrcEnd:
+                unsplitRanges.add (srcRange.start, rmSrcEnd - srcRange.start)
+                unsplitRanges.add (rmSrcEnd, srcRangeEnd - rmSrcEnd)
+                split = true
+                break
+        
+        if not split:
+            splitRanges.add srcRange
+    
+    # process the ranges
+
+    for splitRange in splitRanges.mItems:
+        for rm in rangeMaps:
+            if splitRange.start >= rm.srcStart and splitRange.start < rm.srcStart + rm.len:
+                splitRange.start = rm.destStart + (splitRange.start - rm.srcStart)
+                break
+
+    sourceRanges = splitRanges
 
 var i = 3
 
@@ -24,99 +67,13 @@ while i < lines.len:
         rangeMaps.add r
         i.inc
     else:
-        # split the ranges so that every range is fully within a range map
-
-        var unsplitRanges = sourceRanges
-        var splitRanges: seq[Range]
-
-        while unsplitRanges.len > 0:
-            let srcRange = unsplitRanges.pop
-
-            assert srcRange.len > 0
-
-            var split = false
-
-            for rm in rangeMaps:
-                let srcRangeEnd = srcRange.start + srcRange.len
-                let rmSrcEnd = rm.srcStart + rm.len
-
-                # range crosses low end of range map
-
-                if srcRange.start < rm.srcStart and srcRangeEnd > rm.srcStart:
-                    unsplitRanges.add (srcRange.start, rm.srcStart - srcRange.start)
-                    unsplitRanges.add (rm.srcStart, srcRangeEnd - rm.srcStart)
-                    split = true
-                    break
-
-                # range crosses high end of range map
-            
-                if srcRange.start < rmSrcEnd and srcRangeEnd > rmSrcEnd:
-                    unsplitRanges.add (srcRange.start, rmSrcEnd - srcRange.start)
-                    unsplitRanges.add (rmSrcEnd, srcRangeEnd - rmSrcEnd)
-                    split = true
-                    break
-            
-            if not split:
-                splitRanges.add srcRange
-        
-        # process the ranges
-
-        for splitRange in splitRanges.mItems:
-            for rm in rangeMaps:
-                if splitRange.start >= rm.srcStart and splitRange.start < rm.srcStart + rm.len:
-                    splitRange.start = rm.destStart + (splitRange.start - rm.srcStart)
-                    break
-
-        sourceRanges = splitRanges
+        mapSourceRanges()
 
         # reset range maps and move to next range list
 
         rangeMaps.setLen 0
         i.inc 2
 
-
-
-# split the ranges so that every range is fully within a range map
-
-var unsplitRanges = sourceRanges
-var splitRanges: seq[Range]
-
-while unsplitRanges.len > 0:
-    let srcRange = unsplitRanges.pop
-
-    var split = false
-
-    for rm in rangeMaps:
-        let srcRangeEnd = srcRange.start + srcRange.len
-        let rmSrcEnd = rm.srcStart + rm.len
-
-        # range crosses low end of range map
-
-        if srcRange.start < rm.srcStart and srcRangeEnd > rm.srcStart:
-            unsplitRanges.add (srcRange.start, rm.srcStart - srcRange.start)
-            unsplitRanges.add (rm.srcStart, srcRangeEnd - rm.srcStart)
-            split = true
-            break
-
-        # range crosses high end of range map
-    
-        if srcRange.start < rmSrcEnd and srcRangeEnd > rmSrcEnd:
-            unsplitRanges.add (srcRange.start, rmSrcEnd - srcRange.start)
-            unsplitRanges.add (rmSrcEnd, srcRangeEnd - rmSrcEnd)
-            split = true
-            break
-    
-    if not split:
-        splitRanges.add srcRange
-
-# process the ranges
-
-for splitRange in splitRanges.mItems:
-    for rm in rangeMaps:
-        if splitRange.start >= rm.srcStart and splitRange.start < rm.srcStart + rm.len:
-            splitRange.start = rm.destStart + (splitRange.start - rm.srcStart)
-            break
-
-sourceRanges = splitRanges
+mapSourceRanges()
 
 echo sourceRanges.min.start
