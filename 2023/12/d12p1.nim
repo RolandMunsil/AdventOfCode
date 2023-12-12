d40315d34afe93f2f08a199aaf84edc7bb688944
Roland Munsil <roland@munsil.com>
2023-12-12 02:09:04 -0800

    2023 day 12

---
+++ 2023/12/d12p1.nim
@@ -0,0 +1,56 @@
+import strscans
+import sequtils
+import strutils
+import bitops
+
+
+for line in lines "input.txt":
+    var sum = 0 
+    let (_, row, sizesStr) = line.scanTuple("$* $*$.")
+    let sizes = sizesStr.split(',').mapIt(it.parseInt)
+
+    let nUnknown = row.countIt(it == '?')
+
+    for k in 0..<(1 shl nUnknown):
+        var curSizes = sizes
+        var curBit = nUnknown - 1
+
+        var chPrev = '.'
+        var matches = true
+
+        for ch in row:
+            var chR = ch
+            if ch == '?':
+                assert curBit >= 0
+                if k.testBit(curBit):
+                    chR = '#'
+                else:
+                    chR = '.'
+                curBit.dec
+
+            if chR == '.' and chPrev == '#':
+                if curSizes[0] == 0:
+                    curSizes.delete(0)
+                else:
+                    matches = false
+                    break
+            elif chR == '#':
+                if curSizes.len == 0:
+                    matches = false
+                    break
+                curSizes[0].dec
+
+            chPrev = chR
+        
+        if chPrev == '#':
+            if curSizes[0] == 0:
+                curSizes.delete(0)
+            else:
+                matches = false
+        
+        if curSizes.len != 0:
+            matches = false
+
+        if matches: sum.inc
+
+    echo sum
---
+++ 2023/12/d12p2.nim
@@ -0,0 +1,82 @@
+import strscans
+import sequtils
+import strutils
+import tables
+
+func usedSpace(bin: seq[int]): int = bin.foldl(a + b, 0) + bin.len - 1
+
+var tabs = ""
+
+var table: Table[(seq[int], string), int]
+
+proc nArrangements(sizes: seq[int], row: string): int = 
+    let key = (sizes, row)
+    if table.hasKey(key):
+        return table[key]
+    
+    let wiggleRoom = row.len - usedSpace(sizes)
+
+    # debugEcho sizes, ' ', row
+
+    # if wiggleRoom < 0:
+    #     return 0
+
+    let iMaxSize = sizes.maxIndex
+    let maxSize = sizes[iMaxSize]
+    let sizesBelow = if iMaxSize > sizes.low: sizes[0..iMaxSize-1] else: @[]
+    let sizesAbove = if iMaxSize < sizes.high: sizes[iMaxSize+1..^1] else: @[]
+    let iChLowStart = sizesBelow.foldl(a + b, 0) + sizesBelow.len
+    let iChHighStart = iChLowStart + wiggleRoom
+
+    result = 0
+
+    tabs &= '\t'
+
+    for iChStart in iChLowStart..iChHighStart:
+        let iChEnd = iChStart + maxSize - 1
+        if iChStart > row.low and row[iChStart - 1] == '#':
+            continue
+        if iChEnd < row.high and row[iChEnd + 1] == '#':
+            continue
+        if row[iChStart..iChEnd].anyIt(it == '.'):
+            continue
+
+        var thisResult = 1
+
+        if sizesBelow.len == 0:
+            if iChStart-2 >= row.low and row[row.low..iChStart-2].anyIt(it == '#'):
+                thisResult = 0
+        else:
+            let rowBelow = row[row.low..iChStart - 2]
+            thisResult *= nArrangements(sizesBelow, rowBelow)
+        
+        if sizesAbove.len == 0:
+            if iChEnd+2 <= row.high and row[iChEnd+2..row.high].anyIt(it == '#'):
+                thisResult = 0
+        else:
+            let rowAbove = row[iChEnd + 2..row.high]
+            thisResult *= nArrangements(sizesAbove, rowAbove)
+
+        result += thisResult
+
+    tabs = tabs[0..tabs.high-1]
+
+    table[key] = result
+    # debugEcho tabs,row, " + ", sizes, " => ", result
+
+var sum = 0
+
+for line in lines "input.txt":
+    let (_, rowBit, sizesStr) = line.scanTuple("$* $*$.")
+    let sizesBit = sizesStr.split(',').mapIt(it.parseInt)
+
+    var row = rowBit & '?' & rowBit & '?' & rowBit & '?' & rowBit & '?' & rowBit
+    let sizes = sizesBit & sizesBit & sizesBit & sizesBit & sizesBit
+
+    row = row.split('.').filterIt(it.len > 0).join(".")
+
+    sum.inc nArrangements(sizes, row)
+    echo sum, " tbl: ", table.len
+    # discard readLine(stdin)
+
+echo sum
