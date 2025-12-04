9d42fb1193114a86d73db2fcb7a860a745f52724
Roland Munsil <roland@munsil.com>
2025-12-04 09:42:35 -0800

    Even further simplification of d3p2

--- 2025/03/d03p2.rb
+++ 2025/03/d03p2.rb
@@ -1,17 +1,9 @@
 banks = File.read('2025/03/input.txt').lines.map { |line| line.strip.chars.map(&:to_i) }
 
 def best_joltage(bank, digits_needed)
-  9.downto(1) do |digit|
-    first_index = bank[..-digits_needed].index digit
-    next if first_index.nil?
-    return digit if digits_needed == 1
-
-    next_start_index = first_index + 1
-    additional_digits_needed = digits_needed - 1
-    next if (bank.length - next_start_index) < additional_digits_needed
-
-    return best_joltage(bank[next_start_index..], additional_digits_needed) + (digit * 10.pow(digits_needed - 1))
-  end
+  digit = bank[..-digits_needed].max
+  return digit if digits_needed == 1
+  return best_joltage(bank[(bank.index(digit) + 1)..], digits_needed - 1) + (digit * 10.pow(digits_needed - 1))
 end
 
 total_joltage = banks.sum do |bank|
