2251a4bf97b50f218a517ac10509030ec461eb9a
Roland Munsil <roland@munsil.com>
2025-12-04 09:50:51 -0800

    Last change to d3p2 I promise

--- 2025/03/d03p2.rb
+++ 2025/03/d03p2.rb
@@ -6,8 +6,16 @@ def best_joltage(bank, digits_needed)
   return best_joltage(bank[(bank.index(digit) + 1)..], digits_needed - 1) + (digit * 10.pow(digits_needed - 1))
 end
 
-total_joltage = banks.sum do |bank|
-  best_joltage(bank, 12)
-end
+puts(banks.sum { |bank| best_joltage(bank, 12) })
 
-puts total_joltage
+# Non-recursive version
+
+# puts (banks.sum do |bank|
+#   total = 0
+#   12.downto(1).each do |digits_needed|
+#     digit = bank[..-digits_needed].max
+#     total = (total * 10) + digit
+#     bank = bank[(bank.index(digit) + 1)..]
+#   end
+#   total
+# end)
